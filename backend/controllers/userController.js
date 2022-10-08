c4a50740ab2b1f5475ec0fd6677a48d0c7b59a09
Johnpaul <pauledozie004@gmail.com>
2022-10-08 13:01:52 +0100

    successfully signup functions and added jwt

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,12 +1,21 @@
 const User = require('../model/userModel')
+const jwt = require('jsonwebtoken')
+
+const createToken = (_id) => {
+    return jwt.sign({_id}, process.env.SECRET_KEY, {expiresIn: '3d'})
+}
 
 // signup user
 const signupUser = async (req, res) => {
     const {username,email,password,role} = req.body
-    
+
     try{
         const user = await User.signup(username,email,password,role)
-        res.status(200).json({user})
+
+        // create a token
+        const token = createToken(user._id)
+    
+        res.status(200).json({email, token})
     } catch(error){
         res.status(400).json({error: error.message})
     }
